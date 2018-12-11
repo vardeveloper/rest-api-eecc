5485a708c7e586055f20444d6f8d236dc8897a1e
José Ayón <jayon@funciton.com>
2018-12-10 19:55:35 -0500

    Agrego nuevos mailings

--- frontend/handler.js
+++ frontend/handler.js
@@ -1,5 +1,9 @@
 var handler = {};
 
+if (process.argv && process.argv.indexOf('--deploy') != -1) {
+  handler.deploy = true;
+}
+
 handler.static_url = function(src, base) {
   return [(base || 'static'), (src || '')].join('/');
 };
